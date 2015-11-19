d46bfd26a0558c084e1619acade41187ea899927
tomek <tomek@9.164.180.44>
2015-11-19 23:10:25 +0100

    .

--- configManager/main.py
+++ configManager/main.py
@@ -8,7 +8,7 @@ timeframe = sys.argv[3]
 quality = sys.argv[4]
 structure.config['templates']['tv']['configure_series']['settings']['quality'] = quality
 structure.config['templates']['tv']['configure_series']['settings']['timeframe'] = timeframe
-structure.config['templates']['tv']['configure_series']['settings']['from']['trakt_list']['username'] = user
-structure.config['templates']['tv']['configure_series']['settings']['from']['trakt_list']['list'] = list
+structure.config['templates']['tv']['configure_series']['from']['trakt_list']['username'] = user
+structure.config['templates']['tv']['configure_series']['from']['trakt_list']['list'] = list
 with open(structure.path, 'w') as outfile:
     outfile.write(yaml.dump(structure.config, default_flow_style=True))
