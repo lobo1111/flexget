f024c9cde96e4990e4ae7602deca4db5950e0982
tomek <tomek@9.164.180.44>
2015-11-19 22:43:27 +0100

    .

--- configManager/main.py
+++ configManager/main.py
@@ -3,15 +3,12 @@ import save
 import os.path
 import sys
 
-if not os.path.isfile(structure.path):
-    user = sys.argv[1]
-    list = sys.argv[2]
-    timeframe = sys.argv[3]
-    quality = sys.argv[4]
-    with open(structure.path, 'rw') as ymlfile:
-        cfg = yaml.load(ymlfile)
-        cfg['templates']['tv']['configure_series']['settings']['default']['quality'] = quality
-        cfg['templates']['tv']['configure_series']['settings']['default']['timeframe'] = timeframe
-        cfg['templates']['tv']['configure_series']['settings']['default']['from']['trakt_list']['username'] = user
-        cfg['templates']['tv']['configure_series']['settings']['default']['from']['trakt_list']['list'] = list
-        save.saveConfig(cfg)
+user = sys.argv[1]
+list = sys.argv[2]
+timeframe = sys.argv[3]
+quality = sys.argv[4]
+structure.default['templates']['tv']['configure_series']['settings']['default']['quality'] = quality
+structure.default['templates']['tv']['configure_series']['settings']['default']['timeframe'] = timeframe
+structure.default['templates']['tv']['configure_series']['settings']['default']['from']['trakt_list']['username'] = user
+structure.default['templates']['tv']['configure_series']['settings']['default']['from']['trakt_list']['list'] = list
+save.saveConfig(structure.default)
